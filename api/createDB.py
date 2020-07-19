6d50bd562981e788c8a1b9572b2f33b4de19069a
Emikinder <emikinderk@gmail.com>
2020-07-19 17:36:26 -0300

    db name

--- api/createDB.py
+++ api/createDB.py
@@ -13,13 +13,13 @@ def createDatabase():
                                 password="e3231441221")
     try:
         cursor = conn.cursor()
-        cursor.execute("CREATE DATABASE IF NOT EXISTS mt1")
+        cursor.execute("CREATE DATABASE IF NOT EXISTS mutants")
     except:
         print('Error createDatabase.')
 
 def createTable():
     conn = mysql.connector.connect(host="localhost", user="root",
-                                password="e3231441221", database="mt1")
+                                password="e3231441221", database="mutants")
     try:
         cursor = conn.cursor()
         cursor.execute("CREATE TABLE `dna` (`id` int NOT NULL AUTO_INCREMENT,`dna` longtext NOT NULL,`isMutant` tinyint(1) NOT NULL DEFAULT '0',PRIMARY KEY (`id`),UNIQUE KEY `id_UNIQUE` (`id`))")
